8077e3f66d25848e388727d7018efe6dd037d54a
liudao_glb <liudaokk@163.com>
2020-04-10 19:03:33 +0800

    使用了安全认证+spring security使用了数据库的认证

---
+++ bms_ssm_service/src/main/java/com/liudao/service/IUserService.java
@@ -0,0 +1,11 @@
+package com.liudao.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * @author:liudao
+ * @company:null
+ * @date: 2020/4/10 - 12:02
+ */
+public interface IUserService extends UserDetailsService {
+}
